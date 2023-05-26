98f8b649a98144057d93b71302a3e94b531ca674
Emiliano <emilianobz546@gmail.com>
2023-05-26 11:52:32 -0300

    Introduccion

--- MasterClass/Assets/Scenes/Scripts/Variables.cs
+++ MasterClass/Assets/Scenes/Scripts/Variables.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Tipos de datos(Valor)
+    public int myInt;
+    public float myFloat;
+    public double myDouble;
+    public bool myBool;
+    public string myString;
+    public char myChar;
+    public Vector3 myStructVector;
+    public Quaternion myStructQuaternion;
 
-    // Update is called once per frame
-    void Update()
+    //Tipos de datos(Referencia)
+    public Transform myClassTransform;
+    public GameObject myClassGameObject;
+    public Variables myClass;
+
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        //Tipos de datos(Valor)
+        myInt = 0;
+        myFloat = 0;
+        myDouble = 0;
+        myBool = false;
+        myString = string.Empty;
+        myChar = '\0';
+        myStructVector = new Vector3(1,2,3);
+        myStructQuaternion = Quaternion.identity;
+
+        //Tipos de datos(Referencia)
+        myClass = this;
+        myClassGameObject = gameObject;
+        myClassTransform = gameObject.transform;
     }
 }
