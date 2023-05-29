6f925e69e47c4c6ab9de41fef4a3bf8bb224e19d
Emiliano <emilianobz546@gmail.com>
2023-05-28 22:36:11 -0300

    Generic

--- MasterClass/Assets/Scenes/Scripts/06_InterfacesYGenerics/Generics.cs
+++ MasterClass/Assets/Scenes/Scripts/06_InterfacesYGenerics/Generics.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Generics : MonoBehaviour
 {
+    public List<int> numbers;
+    public List<string> words;
 
+    private void Start()
+    {
+        words = ReverseList(words);
+        numbers = ReverseList(numbers);
+    }
+
+    private List<T> ReverseList<T>(List<T> listToReverse)
+    {
+        List<T> reversedList = new List<T>();
+
+        for (int i = listToReverse.Count - 1; i >= 0; i--)
+        {
+            reversedList.Add(listToReverse[i]);
+        }
+
+        return reversedList;
+    }
 }
